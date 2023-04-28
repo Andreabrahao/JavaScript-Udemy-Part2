38bce02c121c6e81f80cd48967ebf38cea5de5e4
André Abrahão <abrahaoam@gmail.com>
2023-04-28 15:17:05 -0300

    Continuacao dos estudos

---
+++ aula 18 (switch)/scripts.js
@@ -0,0 +1,21 @@
+let nome = "Andre";
+
+switch(nome){
+    case "Andre":
+        console.log("O nome e Andre");
+        break;
+    case "zezin":
+        console.log("O nome e zezin");
+        break;
+    default:
+        console.log("O nome nao foi encontrado");
+        break;
+}
+
+// Maneira mais simples de escrever.
+
+// if(nome == "Andre"){
+//     console.log("O nome e Andre")
+// } else {
+//     console.log("O nome nao foi encontrado");
+// }
